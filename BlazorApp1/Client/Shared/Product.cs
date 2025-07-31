deaf85d897bf3ab1a1aa0b5b4d765e8c2d6d2fbc
niloufar <niloufar.ebrahimizade@gmail.com>
2025-07-31 06:32:15 +0330

    Add project files.

---
+++ BlazorApp1/Client/Shared/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlazorApp2.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+
+    }
+}
